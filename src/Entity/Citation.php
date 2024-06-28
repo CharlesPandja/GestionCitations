24cefb0a9fd3dfc63598a80906d2a50271a785f3
Charles <pandjacharles@gmail.com>
2024-06-28 16:53:08 +0200

    Gestion de citations célèbres

---
+++ src/Entity/Citation.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CitationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CitationRepository::class)]
+class Citation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $texte = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $auteur = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTexte(): ?string
+    {
+        return $this->texte;
+    }
+
+    public function setTexte(string $texte): static
+    {
+        $this->texte = $texte;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(string $auteur): static
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+}
